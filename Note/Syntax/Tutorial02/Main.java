0c9fe8f8c1d51997a382943f3f802d00abb756ad
Jeff Hwang <67028384+jhwa426@users.noreply.github.com>
2021-07-21 12:00:51 +1200

    Add files via upload

---
+++ Note/Syntax/Tutorial02/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+// 1.variable
+	
+	
+////	1. declare variable
+////	int intType = 100;
+////	double doubleType = 150.5;
+////	String stringType = "Jeff";
+////	
+////	System.out.println(intType);
+////	System.out.println(doubleType);
+////	System.out.println(stringType);
+//	
+////	int r = 30;
+////	System.out.println(r * r * PI);
+//	
+////	int a = 1;
+////	int b = 2;
+////	System.out.println("a + b = " + (a+b));
+////	System.out.println("a - b = " + (a-b));
+////	System.out.println("a * b = " + (a*b));
+////	System.out.println("a / b = " + (a/b));
+////	System.out.println("a % b = " + (a%b));
+//	
+////	double b = 0.5;
+////	int a = (int) (b + 0.5);
+////	System.out.println(a);
+}
