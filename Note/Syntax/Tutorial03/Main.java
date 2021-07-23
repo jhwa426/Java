bcd9aa97143ce3d28480f22e43245dcb12744440
Jeff Hwang <67028384+jhwa426@users.noreply.github.com>
2021-07-23 23:32:54 +1200

    Add files via upload

---
+++ Note/Syntax/Tutorial03/Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+// 3. Data Type
+		
+//		double a = 10.3;
+//		double b = 9.6;
+//		double c = 10.1;
+//		
+//		System.out.println((a + b + c ) / 3);
+		
+//		for (char i = 'a'; i <= 'z'; i++) {
+//			System.out.print(i + " ");
+//		}
+//		
+//		for (int a = 0; a <= 10; a++) {
+//			System.out.println(a);
+//		}
+
+		
+//		int a = 200;
+//		System.out.println("Decimal : " + a);
+//		System.out.format("Octal : %o\n", a);
+//		System.out.format("Hexadecimal : %x", a);
+		
+		
+		String name = "Jeff Hwang";
+		System.out.println(name);
+		System.out.println(name.substring(0,4)); //start from 0 and print before the number (same with Python)
+		
+	}
+
+}
