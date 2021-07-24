aecc36e739bc39754e9e0e4fa5af6872ba4b921b
Jeff Hwang <67028384+jhwa426@users.noreply.github.com>
2021-07-24 23:53:03 +1200

    Add files via upload

---
+++ Note/Syntax/Tutorial04/Main.java
@@ -0,0 +1,51 @@
+// 4. Operator
+public class Main {
+	
+	final static int SECOND = 1000;
+	public static void main(String[] args) {
+
+//		int minute = SECOND / 60;
+//		int second = SECOND % 60;
+//		
+//		System.out.println(minute + " minutes " + second + " seconds");
+		
+//		int a = 10;
+//		System.out.println("Current a is a " + a);
+//		System.out.println("Current a is a " + ++a);
+//		System.out.println("Current a is a " + a++);
+//		System.out.println("Current a is a " + a);
+		
+//		System.out.println(1 % 3);
+//		System.out.println(2 % 3);
+//		System.out.println(3 % 3);
+//		System.out.println(4 % 3);
+//		System.out.println(5 % 3);
+//		System.out.println(6 % 3);
+		
+//		int a = 50;
+//		int b = 50;
+//				
+//		System.out.println(a == b);
+//		System.out.println(a > b);
+//		System.out.println(a < b);
+//		System.out.println((a == b) && (a ==50));
+//		System.out.println((a == b) && (a < 50));
+//		System.out.println((a == b) && !(a < 50));
+		
+//		int x = 50;
+//		int y = 60;
+//		
+//		System.out.println("The biggest number is " + max(x,y) + ".");
+		
+			double a = Math.pow(3.0, 20.0);
+			System.out.println("3 power of 20 is " + a + ".");
+
+		}
+	
+		static int max(int a, int b) {
+			int result = (a > b) ? a : b;
+			return result;
+			
+	}
+
+}
