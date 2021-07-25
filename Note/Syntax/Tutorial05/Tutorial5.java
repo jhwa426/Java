99974fe47393b926719e141edd35c26de602c20d
Jeff Hwang <67028384+jhwa426@users.noreply.github.com>
2021-07-25 18:15:44 +1200

    Add files via upload

---
+++ Note/Syntax/Tutorial05/Tutorial5.java
@@ -0,0 +1,37 @@
+//5. IF statement and for statement
+public class Tutorial5 {
+
+	public static void main(String[] args) {
+		
+//		int i = 20;
+//		i++;
+//		i += 1;
+//		i = i + 1;
+//		System.out.println(i);
+//		System.out.println((i < 100) && (i < 200));
+		
+//		int i = 20;
+//		
+//		if (i>= 15) {
+//			i += 1;
+//			System.out.println(i);
+//		}
+//		
+//		
+		//2
+		String a = "Hello World!";
+		
+		if (a.contains("World")) {
+			System.out.println("You are right!");
+		}
+		else {
+			System.out.println("Wrong answer!");
+		}	
+		
+		
+
+		
+		
+	}
+
+}
