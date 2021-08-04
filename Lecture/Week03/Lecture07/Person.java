6b81acfb65e692cd794498470b3e07d659f9fae2
Jeff Hwang <67028384+jhwa426@users.noreply.github.com>
2021-08-04 22:51:35 +1200

    Update Person.java

--- Lecture/Week03/Lecture07/Person.java
+++ Lecture/Week03/Lecture07/Person.java
@@ -1,8 +1,16 @@
 class Person {
 	private String name = "";
 	private int age = 1;
+	private double weight;
+	private double height;
 	
-	public Person() {
+	public Person() {}
+	
+	public Person(String name, int age, double weight, double height) {
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+		this.height = height;
 	}
 	
 	public Person(String n, int a) {
@@ -10,23 +18,62 @@ class Person {
 		age = a;
 	}
 	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setAge(int age) {
+		if ((age >= 0) && (age <= 120)) {
+			this.age = age;
+		}
+	}
+	
+	public void growOlder() {
+		this.age += 1;
+	}
+	
+	public boolean isOfLegalAge() {
+		if(this.age >= 18) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	public double getBodyMassIndex() {
+		double bmi = ((this.weight)/(this.height*this.height));
+		return bmi;
+	}
+	
+	
 	public String toString() {
-		String result = String.format("%s(%d)", name, age);
+		String result = String.format("%s(%d),BMI=%.2f", name, age, getBodyMassIndex());
 		return result;
 	}
 	
 	public static void main(String[] args) {
-		
-//		Person p1 = new Person();
-//		System.out.println(p1);
-		
-		Person p1 = new Person("Michael", 21);
-		System.out.println(p1);
-		Person p2 = new Person("Jason", 15);
-		System.out.println(p2);
-		Person p3 = new Person();
-		System.out.println(p3);
 
+		// Q08
+		Person p1 = new Person("Michael", 17, 78, 1.7);
+		Person p2 = new Person("Jason", 15, 52.5, 1.54);
+		System.out.println(p1);
+		System.out.println(p2);
 	}
 
 }
