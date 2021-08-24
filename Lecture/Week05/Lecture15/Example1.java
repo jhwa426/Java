c6b46f52eb942bf05cd6bec01acc914f2e51e0cf
Jeff <wodud6359@gmail.com>
2021-08-25 00:12:01 +1200

    Lecture15

---
+++ Lecture/Week05/Lecture15/Example1.java
@@ -0,0 +1,7 @@
+public class Example1 {
+    public static void main(String[] args) {
+        int value = 0;
+        int answer = 100 / value;
+    }
+}
+  
---
+++ Lecture/Week05/Lecture15/Example2.java
@@ -0,0 +1,8 @@
+public class Example2 {
+    public static void main(String[] args) {
+        int[] array = new int[1];
+        array[0] = 1;
+        array[1] = 2;
+        System.out.println("This will not be printed");
+    }
+}
