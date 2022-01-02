72365241581bb3037477b9d457e51fb011e2908d
Jeff Hwang <wodud6359@gmail.com>
2022-01-02 16:06:55 +1300

    updates

---
+++ Assignment/COMPSCI 230 Test/src/Test.java
@@ -0,0 +1,14 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Test {
+
+	public static void main(String[] args) {
+		D d1 = new H();
+		d1.jm();
+		
+	}
+
+}
